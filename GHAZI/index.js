8f404117d17a3556721d036e2a3dbc25531d603d
GHAZI ALANZI <105205339+GHAZI-ALANZI@users.noreply.github.com>
2022-06-01 13:07:51 +0200

    ok

---
+++ GHAZI/index.js
@@ -0,0 +1,10 @@
+//inter mediat ex1
+function dis(){
+  let x=document.querySelector('.d3').style.display = "none";
+  let x1=document.querySelector('.d4').style.display = "none";
+  let x2=document.querySelector('.d5').style.display = "none";
+  let x3=document.querySelector('.d9').style.display = "none";
+}
+//ex2
+
+
