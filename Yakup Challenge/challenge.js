c6244cca602e333cb8a311212687b8220dd05fdf
yakupdygn <y.dygn66@hotmail.com>
2022-06-01 15:20:08 +0200

    push yakup challenge and intermediate

---
+++ Yakup Challenge/challenge.js
@@ -0,0 +1,20 @@
+// function over(){
+
+
+// }
+
+// function leave(){
+
+
+// }
+
+
+function clickm(){
+    let y = document.getElementById('circle').style.backgroundColor = "red";
+
+}
+
+// function dbclick(){
+
+
+// } 
